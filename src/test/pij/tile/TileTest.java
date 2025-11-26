27f326137fdbdba4c13508f5b41c71b322892eea
Daniel Papier <dpapier01@student.bbk.ac.uk>
2025-11-26 22:21:15 +0000

    Implement basic Tile class Junit tests

---
+++ src/test/pij/tile/TileTest.java
@@ -0,0 +1,16 @@
+package pij.tile;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TileTest {
+    @Test
+    public void testCreateTile() {
+        Tile tile = new Tile('A',2);
+        assertEquals('A',tile.getLetter());
+        assertEquals(2,tile.getValue());
+    }
+
+
+}
