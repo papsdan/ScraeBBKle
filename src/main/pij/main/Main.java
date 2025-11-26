32ed08608faf78a68a400dc55078285cba52a970
Daniel Papier <dpapier01@student.bbk.ac.uk>
2025-11-26 19:46:16 +0000

    Created basic Tile class with letter and value fields

--- src/main/pij/main/Main.java
+++ src/main/pij/main/Main.java
@@ -1,5 +1,11 @@
 package pij.main;
 
-public class Main {
+import pij.tile.Tile;
 
+public class Main {
+    static void main(String[] args) {
+        Tile t = new Tile('a',2);
+        System.out.println(t.getLetter());
+        System.out.println(t.getValue());
+    }
 }
---
+++ src/main/pij/tile/Tile.java
@@ -0,0 +1,17 @@
+package pij.tile;
+
+public class Tile {
+    private char letter;
+    private int value;
+
+    public Tile(char letter, int value) {
+        this.letter = letter;
+        this.value = value;
+    }
+    public char getLetter() {
+        return letter;
+    }
+    public int getValue() {
+        return value;
+    }
+}
