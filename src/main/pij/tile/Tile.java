48ec975b923cbcaba38073b6ff294a9c331858be
Daniel Papier <dpapier01@student.bbk.ac.uk>
2025-11-28 14:16:52 +0000

    Created wildcard methods in Tile class and JUnit tests

--- src/main/pij/tile/Tile.java
+++ src/main/pij/tile/Tile.java
@@ -1,18 +1,31 @@
 package pij.tile;
 
+
 public class Tile {
     private char letter;
-    private int value;
+    private final int value;
+    private boolean isWildcard;
 
     public Tile(char letter, int value) {
         this.letter = letter;
         this.value = value;
+        if(letter == '_'){
+            this.isWildcard = true;
+        }
     }
     public char getLetter() {
         return letter;
     }
     public int getValue() {
         return value;
+    }
+    public boolean isWildcard() {
+        return isWildcard;
+    }
+    public void setWildcardLetter(char letter) {
+        if(this.isWildcard){
+            this.letter = letter;
+        }
     }
     @Override
     public String toString() {
--- src/test/pij/tile/TileTest.java
+++ src/test/pij/tile/TileTest.java
@@ -1,16 +1,50 @@
 package pij.tile;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class TileTest {
     @Test
-    public void testCreateTile() {
-        Tile tile = new Tile('A',2);
+    public void testTileConstructor() {
+        Tile tile = new Tile('A',1);
         assertEquals('A',tile.getLetter());
-        assertEquals(2,tile.getValue());
-        assertEquals("[A2]",tile.toString());
+        assertEquals(1,tile.getValue());
+        assertFalse(tile.isWildcard());
+    }
+    @Test
+    public void testTileConstructorWildcard() {
+        Tile tile = new Tile('_',8);
+        assertEquals('_',tile.getLetter());
+        assertEquals(8,tile.getValue());
+        assertTrue(tile.isWildcard());
+    }
+    @Test
+    public void testSetWildcardLetter() {
+        Tile wildcardTile = new Tile('_',8);
+        assertEquals('_',wildcardTile.getLetter());
+        assertTrue(wildcardTile.isWildcard());
+        wildcardTile.setWildcardLetter('a');
+        assertEquals('a',wildcardTile.getLetter());
+        assertTrue(wildcardTile.isWildcard());
+
+    }
+    @Test
+    public void testToString() {
+        Tile tile  = new Tile('R',4);
+        assertEquals("[R4]", tile.toString());
+
+    }
+    @Test
+    public void testToStringWildcard() {
+        Tile wildcardTile = new Tile('_',8);
+        assertEquals("[_8]", wildcardTile.toString());
+        wildcardTile.setWildcardLetter('a');
+        assertEquals("[a8]", wildcardTile.toString());
+
+
+
     }
 
 
