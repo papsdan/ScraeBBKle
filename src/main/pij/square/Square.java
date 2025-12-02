7ebebe4cc676adf8f9a0a44c50aeb63766222525
Daniel Papier <dpapier01@student.bbk.ac.uk>
2025-12-02 11:13:52 +0000

    Refactoring Square class to make it an abstract class that can then be used for inheritance by PremiumLetterSquare, PremiumWordSquare and Regular Square

--- src/main/pij/square/Square.java
+++ src/main/pij/square/Square.java
@@ -1,48 +1,11 @@
 package pij.square;
 
-public class Square {
-    private int multiplier;
-    private SquareType squareType;
+public abstract class Square {
 
-    public Square(int multiplier, SquareType squareType) {
-        if (multiplier < -9 || multiplier > 99) {
-            throw new IllegalArgumentException("Premium square value must be between -9 and 99");
-        }
-        this.multiplier = multiplier;
-        if (squareType == SquareType.REGULAR) {
-            throw new IllegalArgumentException("Regular squares are not supposed to have multiples");
-        }
-        this.squareType = squareType;
-    }
+    public abstract int getMultiplier();
 
-    public Square(SquareType squareType) {
-        if (squareType != SquareType.REGULAR) {
-            throw new IllegalArgumentException("Premium tiles must include multiples parameter");
-        }
-        this.squareType = SquareType.REGULAR;
-    }
+    public abstract SquareType getSquareType();
 
-    public int getmultiplier() {
-        return multiplier;
-    }
-    public SquareType getsquareType(){
-        return this.squareType;
-    }
-    @Override
-    public String toString() {
-       String prefix = "";
-        if(this.multiplier >= 0 && this.multiplier <= 9) {
-           prefix = " ";
-       } else {
-           prefix = "";
-        };
-        switch (this.squareType) {
-            case PREMIUM_LETTER:
-                return prefix + this.multiplier + ".";
-            case PREMIUM_WORD:
-                return prefix + this.multiplier + "!";
-            default:
-                return " . ";
-        }
-    }
+    public abstract String getDisplayString();
+
 }
