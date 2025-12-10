82f7cb87f88d8da51690122343ab0d9afcc126ba
Daniel Papier <dpapier01@student.bbk.ac.uk>
2025-12-10 17:49:08 +0000

    Added set and get square methods and print methods, which I will then use in the BoardLoader class to parse the defaultboard or another txt file

--- src/main/pij/board/Board.java
+++ src/main/pij/board/Board.java
@@ -1,19 +1,21 @@
 package pij.board;
+import pij.square.PremiumLetterSquare;
+import pij.square.PremiumWordSquare;
 import pij.square.RegularSquare;
 import pij.square.Square;
 
 public class Board {
-     int cols;
-     int rows;
-     String startingPosition;
-     Square[][] squares;
+    private int cols;
+    private int rows;
+    private String startingPosition;
+    private Square[][] squares;
 
-    public Board(int rows,int cols,  String startingPosition) {
-        if(rows < 10 || rows > 99) {
-            throw new IllegalArgumentException("Rows must be between 10 and 99");
-        }
+    public Board(int cols, int rows, String startingPosition) {
         if(cols < 7 || cols > 26) {
             throw new IllegalArgumentException("Columns must be between 7 and 26");
+        }
+        if(rows < 10 || rows > 99) {
+            throw new IllegalArgumentException("Rows must be between 10 and 99");
         }
         if(cols*rows < 192) {
             throw new IllegalArgumentException("Board must have at least 192 squares");
@@ -46,9 +48,49 @@ public class Board {
         return startingPosition;
     }
 
+    public void setSquare(int row, int column, Square square) {
+        this.squares[row][column] = square;
+    }
+
+    public Square getSquare(int row, int column) {
+        return squares[row][column];
+    }
+
+    public Square getSquareByPosition(String startingPosition){
+        char colChar = startingPosition.charAt(0);
+        int rowNum =  Integer.parseInt(startingPosition.substring(1));
+        int colNum = 0;
+        if (colChar >= 'a' && colChar <= 'z') {
+            colNum = (int) colChar - (int) 'a';
+        }
+        return getSquare(rowNum-1, colNum);
+    }
+
+
+    public void prettyPrint() {
+        for (int i=0;i<this.squares.length;i++) {
+            System.out.println();
+            for (int j=0;j<this.squares[i].length;j++) {
+                System.out.print(getSquare(i,j).getDisplayString());
+            }
+        }
+    }
+
 
     static void main(String[] args) {
-        Board board = new Board(20,10,"d7");
+        Board board = new Board(14,16,"f7");
         System.out.println(board);
-    }
+        
+
+        board.setSquare(2,0,new PremiumWordSquare(10));
+        board.setSquare(1,1,new PremiumLetterSquare(24));
+        board.setSquare(5,2,new PremiumWordSquare(13));
+        board.setSquare(0,0,new PremiumWordSquare(16));
+        board.getSquare(0,0);
+        board.prettyPrint();
+        System.out.println();
+        System.out.println(board.getSquareByPosition("a1").getDisplayString());
+
 }
+    }
+
