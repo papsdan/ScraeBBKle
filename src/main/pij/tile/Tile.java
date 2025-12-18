418251e2f1096cb8d3fef54185c5979e9e851cf3
Daniel Papier <dpapier01@student.bbk.ac.uk>
2025-12-18 12:11:01 +0000

    Adding error handling to Tile class including not allowing setting wildcard character more than once and adding final to parameters that do not change

--- src/main/pij/tile/Tile.java
+++ src/main/pij/tile/Tile.java
@@ -4,34 +4,40 @@ package pij.tile;
 public class Tile {
     private char letter;
     private final int value;
-    private boolean isWildcard;
+    private final boolean isWildcard;
     private boolean isWildcardAssigned;
 
     public Tile(char letter, int value) {
         this.letter = letter;
         this.value = value;
-        if(letter == '_'){
-            this.isWildcard = true;
-        }
+        this.isWildcard = letter == '_';
         this.isWildcardAssigned = false;
     }
+
     public char getLetter() {
         return letter;
     }
+
     public int getValue() {
         return value;
     }
+
     public boolean isWildcard() {
         return isWildcard;
     }
+
     public void setWildcardLetter(char letter) {
-        if(this.isWildcard && !this.isWildcardAssigned){
+        if (this.isWildcard && !this.isWildcardAssigned) {
             this.letter = letter;
             this.isWildcardAssigned = true;
+        } else {
+            throw new RuntimeException("Wildcard letter already set");
         }
     }
+
     @Override
     public String toString() {
         return "[" + letter + value + "]";
     }
+    
 }
