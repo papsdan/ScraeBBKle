81d97299021ad250f1c216374ff5cf3cc579e1df
Daniel Papier <dpapier01@student.bbk.ac.uk>
2025-12-19 12:47:29 +0000

    Added logic to Move class to skip occupied squares. Will need refactoring as duplicated code

--- src/main/pij/board/Move.java
+++ src/main/pij/board/Move.java
@@ -1,6 +1,9 @@
 package pij.board;
 
 import pij.tile.Tile;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Move {
@@ -25,18 +28,65 @@ public class Move {
 
         if(Character.isDigit(firstPositionCharacter)){
             for(Tile tile : tiles){
-                board.getSquare(currentRowIndex,currentColIndex).setTile(tile);
-                currentColIndex++;
+
+                if(!board.getSquare(currentRowIndex,currentColIndex).isSquareOccupied()){
+                    board.getSquare(currentRowIndex,currentColIndex).setTile(tile);
+                    currentColIndex++;
+                } else {
+                    currentColIndex++;
+                    board.getSquare(currentRowIndex,currentColIndex).setTile(tile);
+                }
             }
-        } else{
+        }else{
             for(Tile tile : tiles){
-                board.getSquare(currentRowIndex,currentColIndex).setTile(tile);
-                currentRowIndex++;
+
+                if(!board.getSquare(currentRowIndex,currentColIndex).isSquareOccupied()){
+                    board.getSquare(currentRowIndex,currentColIndex).setTile(tile);
+                    currentRowIndex++;
+                } else {
+                    currentRowIndex++;
+                    board.getSquare(currentRowIndex,currentColIndex).setTile(tile);
                 }
             }
 
-        }
+        }}
+    static void main(String[] args) throws IOException {
+        Board board = BoardLoader.loadFromFile("resources/defaultBoard.txt");
+        List<Tile> tiles = List.of(
+                new Tile('D', 2),
+                new Tile('I', 1),
+                new Tile('N', 1),
+                new Tile('E', 2),
+                new Tile('D', 2)
+        );
 
+        Move move = new Move(board, "d4", tiles);
+        move.placeTile();
+        board.displayBoard();
+
+        List<Tile> tiles2 = List.of(
+                new Tile('T', 1),
+                new Tile('N', 1),
+                new Tile('Z', 9),
+                new Tile('O', 1),
+                new Tile('N', 1)
+        );
+
+        Move move2 = new Move(board, "7c", tiles2);
+        move2.placeTile();
+        board.displayBoard();
+
+        List<Tile> tiles3 = List.of(
+                new Tile('O', 1),
+                new Tile('V', 4),
+                new Tile('E', 2)
+        );
+
+        Move move3 = new Move(board, "4e", tiles3);
+        move3.placeTile();
+        board.displayBoard();
+
+    }
 
     }
 
