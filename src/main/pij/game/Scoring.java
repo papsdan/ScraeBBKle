72b17d523719937b02d39112fdb3c83cbea0a325
Daniel Papier <dpapier01@student.bbk.ac.uk>
2026-01-19 20:11:55 +0100

    Refactor Scoring Class: - removed debug print statements not required - changed premiumLetterScore from public to private as it is an internal helper method

--- src/main/pij/game/Scoring.java
+++ src/main/pij/game/Scoring.java
@@ -1,30 +1,26 @@
 package pij.game;
 
 import pij.board.Board;
-import pij.board.BoardLoader;
 import pij.board.Move;
-import pij.player.HumanPlayer;
-import pij.player.Player;
-import pij.square.PremiumLetterSquare;
-import pij.square.PremiumWordSquare;
+
 import pij.square.Square;
 import pij.square.SquareType;
 import pij.tile.Tile;
 
-import java.io.IOException;
 import java.util.List;
-
+/**
+ * Calculates scores for moves in the game.
+ * Handles premium letter squares, premium word squares and the 7 tile extra bonus scoring.
+ */
 public class Scoring {
-    //board to get multiples for squares
-    //move letters
-    //move position
-    //maybe utilise similar code to placing tiles to get the square multiples and letter, but no skipping. Will need to check left and right to check where the word starts#
-    //methods:
-    // premium letter score (multiplier * letter point),
-    // premium word score (multiplier * sum(all letter points in the word))
-    //--check if multiple letter included in this or origianl letter point--
-    //total score (loops through each square position, checks if regular,premium letter, premium word (if occupied then just assumes its regular as multiplier used), runs the other methods to give number. Then adds to a total score count. Then once finishes returns sum of score)
 
+    /**
+     * Calculates the total score for a move.
+     *
+     * @param move the move to score
+     * @param board the game board
+     * @return the total score for the move
+     */
     public int totalScore(Move move, Board board) {
         int score = 0;
 
@@ -59,14 +55,10 @@ public class Scoring {
                 if(squareType.equals(SquareType.PREMIUM_WORD)) {
                     score += tileValue;
                     wordMultiplier *= board.getSquare(wordStartRowIndex,wordStartColIndex).getMultiplier();
-                    System.out.println(tiles.get(i).getLetter() + "is on a word multiplier = " + board.getSquare(wordStartRowIndex,wordStartColIndex).getMultiplier());
                 } else if (squareType.equals(SquareType.PREMIUM_LETTER)) {
                     score += premiumLetterScore(square,tileValue);
-                    System.out.println(tiles.get(i).getLetter() + "is on a letter multiplier = " + board.getSquare(wordStartRowIndex,wordStartColIndex).getMultiplier());
                 } else {
                     score += tileValue;
-                    System.out.println(tiles.get(i).getLetter() + "is on NOT on a multiplier = " + tileValue);
-
                 }
                 i++;
             }
@@ -79,20 +71,21 @@ public class Scoring {
 
         }
 
-
-        System.out.println("Letter score only: " + score);
-        System.out.println("Word multiplier: " + wordMultiplier);
-        System.out.println("Score = Letter score * wordMultiplier  = " + score);
-
         score *= wordMultiplier;
         if (tiles.size() == 7) {
             score += 60;
         }
         return score;
     }
-    
 
-    public int premiumLetterScore(Square square, int tileValue) {
+    /**
+     * Calculates the score for a tile on a premium letter square.
+     *
+     * @param square the premium letter square
+     * @param tileValue the base value of the tile
+     * @return the multiplied tile value
+     */
+    private int premiumLetterScore(Square square, int tileValue) {
         return square.getMultiplier() * tileValue;
     }
 
