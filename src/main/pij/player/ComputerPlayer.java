ab1b7bd5074bdebac6b0420c770b1dce031c4402
Daniel Papier <dpapier01@student.bbk.ac.uk>
2026-01-19 13:39:30 +0100

    Started working on the makeMove method for the computer. Added the ability to loop through each square to check if occupied, if so, then already know that it is an invalid move

--- src/main/pij/player/ComputerPlayer.java
+++ src/main/pij/player/ComputerPlayer.java
@@ -1,11 +1,14 @@
 package pij.player;
 
 import pij.board.Board;
+import pij.board.BoardLoader;
 import pij.board.Move;
+import pij.square.Square;
 import pij.tile.Tile;
 import pij.tile.TileBag;
 import pij.tile.TileRack;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -18,25 +21,26 @@ public class ComputerPlayer extends Player {
     @Override
     public Move makeMove(Board board) {
             return new Move();
+    }
+
+    public Move makeMoveEnhanced(Board board) {
+        int numberOfTiles = this.getTileRack().getRackCount();
+
+        for(int row = 0; row < board.getRows(); row++){
+            for(int col = 0; col < board.getCols() ; col++){
+                if(!board.getSquare(row,col).isSquareOccupied()){
+                    System.out.println("Square not occupied");
+                }
+            }
         }
+        return new Move();
+    }
 
-    static void main(String[] args) {
+    static void main(String[] args) throws IOException {
+        Board board = BoardLoader.loadFromFile("resources/defaultBoard.txt");
         ComputerPlayer computerPlayer = new ComputerPlayer("ComputerPlayer");
-        String name = computerPlayer.getPlayerName();
-        System.out.println("Player name: " + name);
-        computerPlayer.addScore(2);
-        int score = computerPlayer.getScore();
-        System.out.println("Score: " + score);
-        TileRack tr = computerPlayer.getTileRack();
-        int countTiles = tr.getRackCount();
-
-        System.out.println("Tile Rack: " + tr.getTiles());
-        System.out.println("Tile Rack Count: " + countTiles);
-
-        tr.fillRack(new TileBag());
-        System.out.println("Tile Bag: " + tr.getTiles());
-        tr.fillRack(new TileBag());
-        countTiles = tr.getRackCount();
-        System.out.println("Tile Rack Count: " + countTiles);
+        TileBag bag = new TileBag();
+        computerPlayer.getTileRack().fillRack(bag);
+        computerPlayer.makeMoveEnhanced(board);
     }
 }
