44bc786d92f92589275d0479010a07ba1aeff651
Daniel Papier <dpapier01@student.bbk.ac.uk>
2026-01-25 14:06:38 +0000

    Added javadoc for Move class and getWord method to be used for the system output shown to the player when they make a move

--- src/main/pij/board/Move.java
+++ src/main/pij/board/Move.java
@@ -2,10 +2,13 @@ package pij.board;
 
 import pij.square.Square;
 import pij.tile.Tile;
-
-import java.io.IOException;
 import java.util.List;
 
+/**
+ * Represents a move in the game.
+ * A move consists of the tiles to be placed on the board and the position, which can be either
+ * horizontally or vertically. It can also represent a pass move.
+ */
 public class Move {
     private Board board;
     private String position;
@@ -14,6 +17,16 @@ public class Move {
     private boolean isHorizontal;
     private boolean isPass;
 
+    /**
+     * Constructs a new Move with the board, position and tiles.
+     * The direction is determined by the position format:
+     * - Horizontal = Number first (e.g "7d")
+     * - Vertical = Letter first (e.g "d7")
+     *
+     * @param board the game board
+     * @param position the position for the move
+     * @param tiles the tiles to place
+     */
     public Move(Board board, String position, List<Tile> tiles) {
         this.board = board;
         this.position = position;
@@ -22,15 +35,17 @@ public class Move {
         this.isHorizontal = Character.isDigit(this.position.charAt(0));
     }
 
+    /**
+     * Constructs a pass move, by setting isPass to true if no arguments are
+     */
     public Move() {
         this.isPass = true;
     }
 
-    //if numbers first, then horizontal
-    //if letter first, then downward
-    //Need to also check for letters already in the squares to ensure it skips occupied squares
-    //add validation to check if postion exists, if word exists and if the word can be made with player tiles
-
+    /**
+     * Places the tiles on the board at the specified position.
+     * Skips over occupied squares and builds the word string to get the letters used in the move.
+     */
     public void placeTile() {
 
         int currentColIndex = board.getColumnIndex(position);
@@ -63,6 +78,13 @@ public class Move {
         System.out.println(word);
     }
 
+    /**
+     * Previews the complete word that would be formed by this move.
+     * Includes existing tiles on the board that would connect to the placed tiles.
+     * However, this does not modify the board and is used for validation checks before placing the tile(s).
+     *
+     * @return the complete word that would be formed
+     */
     public String previewWord() {
 
         int currentColIndex = board.getColumnIndex(position);
@@ -116,69 +138,50 @@ public class Move {
         return completePreviewWord.toString();
     }
 
+    /**
+     * Checks if this move is a pass.
+     *
+     * @return true if this is a pass move, false if it is not.
+     */
     public boolean getIsPass() {
         return this.isPass;
     }
+
+    /**
+     * Returns the position for this move.
+     *
+     * @return the position string
+     */
     public String getPosition() {
         return this.position;
     }
 
+    /**
+     * Returns the tiles to be placed in this move.
+     *
+     * @return the list of tiles
+     */
     public List<Tile> getTiles() {
         return tiles;
     }
 
+    /**
+     * Checks if this move is horizontal.
+     *
+     * @return true if horizontal, false if vertical
+     */
     public boolean isHorizontal() {
         return isHorizontal;
     }
 
-    static void main(String[] args) throws IOException {
-        Board board = BoardLoader.loadFromFile("resources/defaultBoard.txt");
-//        List<Tile> tiles = List.of(
-//                new Tile('D', 2),
-//                new Tile('I', 1),
-//                new Tile('N', 1),
-//                new Tile('E', 2),
-//                new Tile('D', 2)
-//        );
-//
-//        Move move = new Move(board, "d4", tiles);
-//        //move.placeTile();
-//        // board.displayBoard();
-//
-//        List<Tile> tiles2 = List.of(
-//                new Tile('T', 1),
-//                new Tile('N', 1),
-//                new Tile('Z', 9),
-//                new Tile('O', 1),
-//                new Tile('N', 1)
-//        );
-//
-//        Move move2 = new Move(board, "7c", tiles2);
-//        //move2.placeTile();
-//        //board.displayBoard();
-
-        List<Tile> tiles3 = List.of(
-                new Tile('O', 1),
-                new Tile('V', 4),
-                new Tile('E', 2)
-        );
-
-        Move move3 = new Move(board, "3c", tiles3);
-        //move3.placeTile();
-        //board.displayBoard();
-
-        board.getSquareByPosition("d3").setTile(new Tile('X', 1));
-//        board.getSquareByPosition("c4").setTile(new Tile('C', 1));
-//        board.getSquareByPosition("b4").setTile(new Tile('B', 1));
-
-        board.displayBoard();
-
-        System.out.println(move3.previewWord());
-
-
-
+    /**
+     * Returns the letters used in the move.
+     *
+     * @return the tile letters used in the move
+     */
+    public String getWord() {
+        return word;
     }
-
 }
 
 
