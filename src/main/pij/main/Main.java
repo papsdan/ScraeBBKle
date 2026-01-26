0abe5a93e55bfa38dacce2e031dce67cb28297b2
Daniel Papier <dpapier01@student.bbk.ac.uk>
2026-01-26 18:04:34 +0000

    Final fix to main class for submission

--- src/main/pij/main/Main.java
+++ src/main/pij/main/Main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
  * Starts the ScraeBBKle game.
  */
 public class Main {
-    static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         Game game = new Game();
         game.play();
     }
